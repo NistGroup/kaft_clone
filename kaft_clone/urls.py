f886f5bc6477839b74fbda4070c73811179694bc
Mert-coderoid <nevi.elric@gmail.com>
2021-10-23 01:21:36 +0300

    Endter

--- kaft_clone/urls.py
+++ kaft_clone/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from page.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('admin/', admin.site.urls),
-]
+  path('', index, name="index"),
+  path('admin/', admin.site.urls),
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
