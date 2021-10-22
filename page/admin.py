52a3967f1340d8821162a1a3315bad4bec97f6cd
Mert-coderoid <nevi.elric@gmail.com>
2021-10-23 00:28:01 +0300

    Carousel Model Added

--- page/admin.py
+++ page/admin.py
@@ -1,11 +1,49 @@
 from django.contrib import admin
 from .models import Page
+from .models import Carousel
 
 # Register your models here.
 
 
 class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "pk",
+        "title",
+        "status",
+        "updated_at",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    list_editable = (
+        "status",
+        "title",
+    )
+
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "title",
+        "status",
+        "updated_at",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    list_editable = (
+        "status",
+        "title",
+    )
+
+
+    # def __str__(self):
+    #     title = self.title
 
 
 admin.site.register(Page, PageAdmin)
+admin.site.register(Carousel)
--- page/models.py
+++ page/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+DEFAULT_STATUS = "draft"
+
 STATUS = [
     ("draft", "Taslak"),
     ("published", "Yayınlandı"),
@@ -12,12 +14,28 @@ class Page(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, default="",)
     content = models.TextField()
-    cover_image = models.ImageField(upload_to="",
-                                    null=True,
-                                    blank=True,
-                                    )
+    cover_image = models.ImageField(
+        upload_to="",
+        null=True,
+        blank=True,
+    )
     status = models.CharField(
-        default="draft",
+        default=DEFAULT_STATUS,
+        choices=STATUS,
+        max_length=10,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Carousel(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    cover_image = models.ImageField(
+        upload_to="carousel",
+        null=True,
+        blank=True,
+    )
+    status = models.CharField(
+        default=DEFAULT_STATUS,
         choices=STATUS,
         max_length=10,
     )
