b6b5e3c98a899fb21f0efd471730a4b6a1c93bdd
NistGroup <nistgrup3@gmail.com>
2021-11-04 17:59:43 +0300

    9.video 34.31

--- page/urls.py
+++ page/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from page.models import Carousel
-from .views import carousel_create
+from .views import carousel_create, carousel_list
 
 urlpatterns = [
-    path('carousel_create/', carousel_create, name="carousel_name",
-    ),
+    path('carousel_create/', carousel_create, name="carousel_name"),
+    path('carousel_list/', carousel_list, name="carousel_title"),
 ]
--- page/views.py
+++ page/views.py
@@ -11,6 +11,18 @@ def index(request):
     context['images'] = Carousel.objects.filter(status="published")
     return render(request, "home/index.html", context)
 
+def carousel_list(request):
+    context = dict()
+    context["carousel"] = Carousel.objects.all().order_by("-pk")
+    return render(request, "manage/carousel_list.html", context)
+
+def carousel_update(request, pk):
+    context = dict()
+    # kaft_clone.com/manage/carousel/1/edit
+    context["item"] = Carousel.objects.get(pk=pk)
+    return render(request, "manage/carousel_update.html", context)
+
+
 def carousel_create(request):
     context = dict()
     context["form"] = CarouselModelForm()
